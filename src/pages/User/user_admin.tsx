5ae0d79d45bce381efe1ed92beb9fbb7cf5ad892
huytbhhh <bahuy353@gmail.com>
2023-10-31 23:29:23 +0700

    #2  đã thêm sản phẩm vào user

--- src/pages/User/user_admin.tsx
+++ src/pages/User/user_admin.tsx
@@ -1,10 +1,59 @@
 export const User_product = ()=> {
     return ( 
-        <div className=" ">
-            <div className="ml-[55px] mt-[30px]">
+        <div>
+            <div className="ml-[55px] mt-[30px] ">
             <h1 className="text-[25px]">Products</h1>
 
-            <div className="flex grid grid-cols-4 gap-[20px] mt-[50px]">
+            <div className=" grid grid-cols-4 gap-[40px] mt-[50px]  ">
+            <div>
+            <img className="w-[270px] h-[270px]" src="https://images.pexels.com/photos/6050919/pexels-photo-6050919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                <div className="bg-slate-400 pt-[20px] text-center w-[270px] h-[100px] rounded-b-md">
+                <p className="pb-[10px]">Nike Air Force 1 NDESTRUKT </p>
+                <p>$ 350</p>
+                </div>
+            </div>
+            <div>
+            <img className="w-[270px] h-[270px]" src="https://images.pexels.com/photos/6050919/pexels-photo-6050919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                <div className="bg-slate-400 pt-[20px] text-center w-[270px] h-[100px] rounded-b-md">
+                <p className="pb-[10px]">Nike Air Force 1 NDESTRUKT </p>
+                <p>$ 350</p>
+                </div>
+            </div>
+            <div>
+            <img className="w-[270px] h-[270px]" src="https://images.pexels.com/photos/6050919/pexels-photo-6050919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                <div className="bg-slate-400 pt-[20px] text-center w-[270px] h-[100px] rounded-b-md">
+                <p className="pb-[10px]">Nike Air Force 1 NDESTRUKT </p>
+                <p>$ 350</p>
+                </div>
+            </div>
+            <div>
+            <img className="w-[270px] h-[270px]" src="https://images.pexels.com/photos/6050919/pexels-photo-6050919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                <div className="bg-slate-400 pt-[20px] text-center w-[270px] h-[100px] rounded-b-md">
+                <p className="pb-[10px]">Nike Air Force 1 NDESTRUKT </p>
+                <p>$ 350</p>
+                </div>
+            </div>
+            <div>
+            <img className="w-[270px] h-[270px]" src="https://images.pexels.com/photos/6050919/pexels-photo-6050919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                <div className="bg-slate-400 pt-[20px] text-center w-[270px] h-[100px] rounded-b-md">
+                <p className="pb-[10px]">Nike Air Force 1 NDESTRUKT </p>
+                <p>$ 350</p>
+                </div>
+            </div>
+            <div>
+            <img className="w-[270px] h-[270px]" src="https://images.pexels.com/photos/6050919/pexels-photo-6050919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                <div className="bg-slate-400 pt-[20px] text-center w-[270px] h-[100px] rounded-b-md">
+                <p className="pb-[10px]">Nike Air Force 1 NDESTRUKT </p>
+                <p>$ 350</p>
+                </div>
+            </div>
+            <div>
+            <img className="w-[270px] h-[270px]" src="https://images.pexels.com/photos/6050919/pexels-photo-6050919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                <div className="bg-slate-400 pt-[20px] text-center w-[270px] h-[100px] rounded-b-md">
+                <p className="pb-[10px]">Nike Air Force 1 NDESTRUKT </p>
+                <p>$ 350</p>
+                </div>
+            </div>
             <div>
             <img className="w-[270px] h-[270px]" src="https://images.pexels.com/photos/6050919/pexels-photo-6050919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                 <div className="bg-slate-400 pt-[20px] text-center w-[270px] h-[100px] rounded-b-md">
@@ -13,32 +62,8 @@ export const User_product = ()=> {
                 </div>
             </div>
 
-            <div>
-            <img className="w-[270px] h-[270px]" src="https://images.pexels.com/photos/6050919/pexels-photo-6050919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                <div className="bg-slate-400 pt-[20px] text-center w-[270px] h-[100px] rounded-b-md">
-                <p className="pb-[10px]">Nike Air Force 1 NDESTRUKT </p>
-                <p>$ 350</p>
-                </div>
             </div>
-
-            <div>
-            <img className="w-[270px] h-[270px]" src="https://images.pexels.com/photos/6050919/pexels-photo-6050919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                <div className="bg-slate-400 pt-[20px] text-center w-[270px] h-[100px] rounded-b-md">
-                <p className="pb-[10px]">Nike Air Force 1 NDESTRUKT </p>
-                <p>$ 350</p>
-                </div>
-            </div>
-
-            <div>
-            <img className="w-[270px] h-[270px]" src="https://images.pexels.com/photos/6050919/pexels-photo-6050919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                <div className="bg-slate-400 pt-[20px] text-center w-[270px] h-[100px] rounded-b-md">
-                <p className="pb-[10px]">Nike Air Force 1 NDESTRUKT </p>
-                <p>$ 350</p>
-                </div>
-            </div>
-
-            </div>
-
+{/* cần xem lại chia 4  */}
 
 
 
